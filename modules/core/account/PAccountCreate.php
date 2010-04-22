09e35bff3b43d8bcde066aa6b74e58e63b79d42e
Mikael Roos <mikael.t.h.roos@gmail.com>
2010-04-22 18:38:10 +0200

    adding procedure for creating new account

---
+++ modules/core/account/PAccountCreate.php
@@ -0,0 +1,109 @@
+<?php
+// ===========================================================================================
+//
+// File: PAccountCreate.php
+//
+// Description: Form to create a new account.
+//
+// Author: Mikael Roos, mos@bth.se
+//
+
+// -------------------------------------------------------------------------------------------
+//
+// Get pagecontroller helpers. Useful methods to use in most pagecontrollers
+//
+$pc = new CPageController();
+$pc->LoadLanguage(__FILE__);
+
+
+// -------------------------------------------------------------------------------------------
+//
+// Interception Filter, controlling access, authorithy and other checks.
+//
+$intFilter = new CInterceptionFilter();
+
+$intFilter->FrontControllerIsVisitedOrDie();
+//$intFilter->UserIsSignedInOrRecirectToSignIn();
+//$intFilter->UserIsMemberOfGroupAdminOrDie();
+
+
+// -------------------------------------------------------------------------------------------
+//
+// Take care of _GET/_POST variables. Store them in a variable (if they are set).
+//
+$account	= $pc->GETisSetOrSetDefault('account', "");
+
+// -------------------------------------------------------------------------------------------
+//
+// Always redirect to latest visited page on success.
+//
+$redirectTo = $pc->SESSIONisSetOrSetDefault('history2');
+
+
+// -------------------------------------------------------------------------------------------
+//
+// Show the login-form
+//
+global $gModule;
+
+$action 			= "?m={$gModule}&amp;p=account-createp";
+$redirect 		= "?m={$gModule}&amp;p=account-settings";
+$redirectFail = "?m={$gModule}&amp;p=account-create";
+$silentLogin 	= "?m={$gModule}&amp;p=loginp";
+
+$htmlMain = <<<EOD
+<h1>{$pc->lang['CREATE_NEW_ACCOUNT_TITLE']}</h1>
+
+<p>{$pc->lang['CHOOSE_NAME_AND_PASSWORD']}</p>
+
+<form action='{$action}' method='POST'>
+<input type='hidden' name='redirect' 			value='{$redirect}'>
+<input type='hidden' name='redirect-fail' value='{$redirectFail}'>
+<input type='hidden' name='silent-login' 	value='{$silentLogin}'>
+
+<fieldset class='accountsettings'>
+<table width='99%'>
+<tr>
+<td><label for="account">{$pc->lang['ACCOUNT_NAME_LABEL']}</label></td>
+<td style='text-align: right;'><input class='account' type='text' name='account' value='{$account}'></td>
+</tr>
+<tr>
+<td><label for="account">{$pc->lang['ACCOUNT_PASSWORD_LABEL']}</label></td>
+<td style='text-align: right;'><input class='password' type='password' name='password1'></td>
+</tr>
+<tr>
+<td><label for="account">{$pc->lang['ACCOUNT_PASSWORD_AGAIN_LABEL']}</label></td>
+<td style='text-align: right;'><input class='password' type='password' name='password2'></td>
+</tr>
+<tr>
+<td colspan='2' style='text-align: right;'>
+<button type='submit' name='submit' value='account-create'>{$pc->lang['CREATE_ACCOUNT']}</button>
+</td>
+</tr>
+</table>
+</fieldset>
+
+</form>
+
+EOD;
+
+$htmlLeft 	= "";
+$htmlRight	= <<<EOD
+<h3 class='columnMenu'>Various ways to sign in</h3>
+<p>
+Later...
+</p>
+
+EOD;
+
+
+// -------------------------------------------------------------------------------------------
+//
+// Create and print out the resulting page
+//
+$page = new CHTMLPage();
+
+$page->printPage($pc->lang['CREATE_NEW_ACCOUNT_TITLE'], $htmlLeft, $htmlMain, $htmlRight);
+exit;
+
+?>
---
+++ modules/core/account/PAccountCreateProcess.php
@@ -0,0 +1,123 @@
+<?php
+// ===========================================================================================
+//
+// File: PAccountCreateProcess.php
+//
+// Description: Create a new account.
+//
+// Author: Mikael Roos, mos@bth.se
+//
+
+
+// -------------------------------------------------------------------------------------------
+//
+// Get pagecontroller helpers. Useful methods to use in most pagecontrollers
+//
+$pc = new CPageController();
+$pc->LoadLanguage(__FILE__);
+
+
+// -------------------------------------------------------------------------------------------
+//
+// Interception Filter, controlling access, authorithy and other checks.
+//
+$intFilter = new CInterceptionFilter();
+
+$intFilter->FrontControllerIsVisitedOrDie();
+//$intFilter->UserIsSignedInOrRecirectToSignIn();
+//$intFilter->UserIsMemberOfGroupAdminOrDie();
+
+
+// -------------------------------------------------------------------------------------------
+//
+// Take care of _GET/_POST variables. Store them in a variable (if they are set).
+//
+$submitAction	= $pc->POSTisSetOrSetDefault('submit');
+$redirect			= $pc->POSTisSetOrSetDefault('redirect');
+$redirectFail	= $pc->POSTisSetOrSetDefault('redirect-fail');
+$silentLogin	= $pc->POSTisSetOrSetDefault('silent-login');
+
+// Always check whats coming in...
+//$pc->IsNumericOrDie($topicId, 0);
+
+
+// -------------------------------------------------------------------------------------------
+//
+// Depending on the submit-action, do whats to be done. If, else if, else, replaces switch.
+// 
+
+
+// -------------------------------------------------------------------------------------------
+//
+// Do some insane checking to avoid misusage, errormessage if not correct.
+// 
+if(false) {
+
+}
+
+
+// -------------------------------------------------------------------------------------------
+//
+// Change the account
+// 
+else if($submitAction == 'account-create') {
+
+	// Get the input and check it
+	$account		= $pc->POSTisSetOrSetDefault('account');
+	$password1	= $pc->POSTisSetOrSetDefault('password1');
+	$password2	= $pc->POSTisSetOrSetDefault('password2');
+
+	if(empty($password1) || empty($password2)) {
+		$pc->SetSessionErrorMessage($pc->lang['PASSWORD_CANNOT_BE_EMPTY']);
+		$pc->RedirectTo($redirectFail);
+	} 
+	else if($password1 != $password2) {
+		$pc->SetSessionErrorMessage($pc->lang['PASSWORD_DOESNT_MATCH']);
+		$pc->RedirectTo($redirectFail);
+	}
+
+	// Execute the database query to make the update
+	$db = new CDatabaseController();
+	$mysqli = $db->Connect();
+
+	// Prepare query
+	$account 	= $mysqli->real_escape_string($account);
+	$password = $mysqli->real_escape_string($password1);
+
+	$query = <<<EOD
+CALL {$db->_['PCreateAccount']}(@accountId, '{$account}', '{$password}', @status);
+SELECT 
+	@accountId AS accountid,
+	@status AS status;
+EOD;
+
+	// Perform the query
+	$results = $db->DoMultiQueryRetrieveAndStoreResultset($query);
+
+	// Get details from resultset
+	$row = $results[1]->fetch_object();
+
+	if($row->status == 1) {
+		$pc->SetSessionErrorMessage($pc->lang['ACCOUNTNAME_ALREADY_EXISTS']);
+		$pc->RedirectTo($redirectFail);	
+	}
+	
+	$results[1]->close();
+	$mysqli->close();
+
+	// Do a silent login and then proceed to $redirect
+	$_SESSION['silentLoginAccount'] 	= $account;
+	$_SESSION['silentLoginPassword'] 	= $password;
+	$_SESSION['silentLoginRedirect'] 	= $redirect;
+	$pc->RedirectTo($silentLogin);
+}
+
+
+// -------------------------------------------------------------------------------------------
+//
+// Default, submit-action not supported, show error and die.
+// 
+die($pc->lang['SUBMIT_ACTION_NOT_SUPPORTED']);
+
+
+?>
