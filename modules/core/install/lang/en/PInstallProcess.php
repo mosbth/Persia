4c45963a2a2e8b033472278552b8062c42562ef3
Mikael Roos <mikael.t.h.roos@gmail.com>
2010-04-23 10:34:32 +0200

    adding procedure for creating new account

---
+++ modules/core/install/lang/en/PInstallProcess.php
@@ -0,0 +1,21 @@
+<?php
+// ===========================================================================================
+//
+// File: PInstallProcess.php
+//
+// Description: Language file
+//
+// Author: Mikael Roos, mos@bth.se
+//
+
+$lang = Array(
+	'DATABASE_INSTALLATION' => "Database installed",
+	'DATABASE_INSTALLATION_LOG' => "Database installation log",
+	'SQL_QUERY' => "SQL query '%s'",
+	'STATEMENTS_SUCCEEDED' => "Statements that succeeded: %d",
+	'ERROR_CODE' => "Errorcode: %1s %2s",
+
+
+);
+
+?>
