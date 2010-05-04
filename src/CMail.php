e5ff7663c0e8727da586b3058650f8d4a7998fe8
Mikael Roos <mikael.t.h.roos@gmail.com>
2010-05-04 15:04:25 +0200

    Enabled sending welcome mail when profile-mailadress is changed.

--- src/CMail.php
+++ src/CMail.php
@@ -41,7 +41,7 @@ class CMail {
 	public function SendMail($to, $from, $subject, $message) {
 
 		// To send HTML mail, the Content-type header must be set
-		$headers  = 'MIME-Version: 1.0' . "\r\n";
+		//$headers  = 'MIME-Version: 1.0' . "\r\n";
 		//$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 		//$headers .= 'To: ' . $to . "\r\n";
 		$headers .= 'From: ' . $from . "\r\n";
