1347ac5abee925a4d9e1b6dbeb7c93d38f0c959c
Mikael Roos <mikael.t.h.roos@gmail.com>
2010-05-26 08:34:11 +0200

    Adding feature for updating metadata on file

---
+++ modules/core/home/lang/en/P404.php
@@ -0,0 +1,20 @@
+<?php
+// ===========================================================================================
+//
+// File: P404.php
+//
+// Description: Language file
+//
+// Author: Mikael Roos, mos@bth.se
+//
+
+$lang = Array(
+	'404_TITLE' => "404 Not Found.",
+	'404_HEADER' => "404 Not Found.",
+	'404_DESCRIPTION' => 
+	"You have used a link that is not supported. The page you are trying to reach does not exist." . 
+	"If you think this is an error, then please report it to the site manager.",
+
+);
+
+?>
---
+++ sql/lang/en/config.php
@@ -0,0 +1,20 @@
+<?php
+// ===========================================================================================
+//
+// File: config.php
+//
+// Description: Language file
+//
+// Author: Mikael Roos, mos@bth.se
+//
+
+$lang = Array(
+
+	// Error messages related to file 
+	'FILE_NO_PERMISSION' => "You have no permission to the file you are trying to access.",
+	'FILE_DOES_NOT_EXISTS' => "The file you are trying to access does not exists.",
+
+);
+
+
+?>
