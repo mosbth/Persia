9b791207a30c1448dc1a04a0ad8ae61bc7409b0f
Mikael Roos <mikael.t.h.roos@gmail.com>
2010-06-15 15:17:39 +0200

    Creating UserControlPanel

---
+++ modules/core/ucp/lang/en/PFileArchive.php
@@ -0,0 +1,28 @@
+<?php
+// ===========================================================================================
+//
+// File: PFileArchive.php
+//
+// Description: Language file
+//
+// Author: Mikael Roos, mos@bth.se
+//
+
+$lang = Array(
+
+	'FILEARCHIVE_FOR' => "Filearchive for: %s",
+	'FILEARCHIVE_DESCRIPTION' => "This is your private filearchive of uploaded files. You can allow others to download the files or link to them from articles, blog- and forum-posts.",
+	'ARCHIVE_IS_EMPTY' => "Your filearchive is currently empty.",
+
+	'FILE_NAME' => "Name",
+	'FILE_TYPE' => "Type",
+	'FILE_SIZE' => "Size",
+	'FILE_CREATED' => "Created",
+	'FILE_MODIFIED' => "Modified",
+
+	'CLICK_TO_EDIT' => "Click to view/edit details of the file",
+
+
+);
+
+?>
---
+++ src/lang/en/CInterceptionFilter.php
@@ -0,0 +1,20 @@
+<?php
+// ===========================================================================================
+//
+// File: CInterceptionFilter.php
+//
+// Description: Language file
+//
+// Author: Mikael Roos, mos@bth.se
+//
+
+$lang = Array(
+
+	'NO_DIRECT_ACCESS' => "No direct access to pagecontroller is allowed.",
+	'NO_AUTHORITY' => "You do not have the authourity to access this page.",
+	'CURRENT_USER_OR_ADMIN' => "You do not have the authourity to access this page.",
+	'USER_DEFINED_FILTER_NOT_ENABLED' => "User defined filter not enabled. No access to this page.",
+
+);
+
+?>
