650a1b999e89146d8bbf46dd15246c96904c97e1
Mikael Roos <mikael.t.h.roos@gmail.com>
2010-06-22 12:33:59 +0200

    Cleaning up to enable first module Dada

---
+++ modules/core/install/lang/en/PInstall.php
@@ -0,0 +1,24 @@
+<?php
+// ===========================================================================================
+//
+// File: PInstall.php
+//
+// Description: Language file
+//
+// Author: Mikael Roos, mos@bth.se
+//
+
+$lang = Array(
+	'INSTALL_PAGE_TITLE' => "Install database",
+	'INSTALL_HEADER' => "Install database",
+	'INSTALL_DESCRIPTION' => "Click below link to remove all contents from the database and create new tables and content from scratch.",
+	'INSTALL_DATABASE_HOST' => "Database host: '%s'",
+	'INSTALL_DATABASE_NAME' => "Database name: '%s'",
+	'INSTALL_DATABASE_PREFIX' => "Prefix for tables: '%s'",
+	'INSTALL_CHANGE_CONFIG' => "date the database config-file (usually config.php) to change the values.",
+	'INSTALL_PROCESS_LINK' => "&not; <a href='%s'>Destroy current database and create from scratch</a>",
+
+);
+
+
+?>
