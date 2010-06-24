68712909f07b0e72cb9fdac34c4e7d2ab27c70ab
Mikael Roos <mikael.t.h.roos@gmail.com>
2010-06-24 12:34:25 +0200

    Added langiuage file

---
+++ modules/core/login/lang/en/PLogoutProcess.php
@@ -0,0 +1,17 @@
+<?php
+// ===========================================================================================
+//
+// File: PLogoutProcess.php
+//
+// Description: Language file
+//
+// Author: Mikael Roos, mos@bth.se
+//
+
+$lang = Array(
+	'WELCOME_BACK' => "%s has signed out. Welcome back another time.",
+
+
+);
+
+?>
